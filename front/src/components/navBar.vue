73900c6c89b5dbded91a8e1a5bcdcd6d3f1dfe3f
Arthur FRIN <frin.arthur@gmail.com>
2024-02-14 12:11:27 +0100

    feat: init nav bar

--- front/src/components/navBar.vue
+++ front/src/components/navBar.vue
@@ -1,17 +1,45 @@
+<script setup lang="ts">
+import iconAdd from '@/components/icons/icon-add.vue';
+import iconArticle from '@/components/icons/icon-article.vue';
+import iconMap from '@/components/icons/icon-map.vue';
+import iconProfile from '@/components/icons/icon-profile.vue';
+
+import { ref, onMounted, onUnmounted } from 'vue';
+
+const currentPage = ref('');
+
+const navigateTo = (page: string) => {
+    currentPage.value = page;
+};
+
+
+</script>
 <template>
     <nav class="nav">
         <ul class="container">
-            <li>
-                <a href="/">Home</a>
+            <li :class="{ 'active': currentPage === 'carte'}">
+                <button @click="navigateTo('carte')" >
+                    <iconMap />
+                    <div>Carte</div>
+                </button>
             </li>
-            <li>
-                <a href="/about">About</a>
+            <li :class="{ 'active': currentPage === 'add'}">
+                <button @click="navigateTo('add')" >
+                    <iconAdd />
+                    <div>Ajouter</div>
+                </button>
             </li>
-            <li>
-                <a href="/contact">Contact</a>
+            <li :class="{ 'active': currentPage === 'article'}">
+                <button @click="navigateTo('article')" >
+                    <iconArticle />
+                    <div>Articles</div>
+                </button>
             </li>
-            <li>
-                <a href="/login">Login</a>
+            <li :class="{ 'active': currentPage === 'profile'}">
+                <button @click="navigateTo('profile')" >
+                    <iconProfile />
+                    <Div>Profile</Div>
+                </button>
             </li>
         </ul>
     </nav>
@@ -24,6 +52,8 @@
         position: fixed;
         bottom: 0;
         width: 100%;
+        font-size: 12px;
+        font-weight: 500;
 
         .container {
             display: flex;
@@ -32,10 +62,19 @@
 
             li {
                 list-style: none;
+                stroke: var(--text-color-secondary);
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-direction: column;
                 a {
                     text-decoration: none;
-                    color: var(--text-secondary);
+                    color: var(--text-color-secondary);
                     
+                }
+
+                &.active {
+                    stroke: red
                 }
             }
         }
