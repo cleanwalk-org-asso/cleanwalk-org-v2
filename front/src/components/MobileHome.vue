0851fd23a1a41cb722a750c73d4acd95213a940c
Arthur FRIN <118976445+Arcoss03@users.noreply.github.com>
2024-02-24 22:42:19 +0100

    feat: modif top bar

--- front/src/components/MobileHome.vue
+++ front/src/components/MobileHome.vue
@@ -5,6 +5,7 @@ import L, { LatLng, LatLngBounds, Map, type PointExpression } from "leaflet";
 import { ref, type Ref, onMounted, nextTick } from "vue";
 import iconLeftArrow from "@/components/icons/icon-left-arrow.vue";
 import iconSearch from "@/components/icons/icon-search.vue";
+import iconInfo from "./icons/icon-info.vue";
 import { useCleanwalkStore } from '@/stores/CleanwalkStore';
 import iconClock from './icons/icon-clock.vue';
 import iconMiniMap from './icons/icon-mini-map.vue';
@@ -19,7 +20,7 @@ let centerMap = ref([48.866667, 2.333333]);
 let center: Ref<PointExpression> = ref([48.866667, 2.333333]);
 let mapInstance: Ref<Map | null> = ref(null);
 let cardListBool = ref(false);  //to display or not the cleanwalk list
-const cleanwalkListContainer =  ref<HTMLElement | null>(null); //ref to the html cleanwalk list container
+const cleanwalkListContainer = ref<HTMLElement | null>(null); //ref to the html cleanwalk list container
 
 const testCleanwalkList = ref([
     {
@@ -157,7 +158,7 @@ function slideUp() {
     if (draggableCard.value) {
         draggableCard.value.style.bottom = '65px'; // Modifiez cette valeur selon la hauteur désirée
     }
-    }
+}
 
 function slideDown() {
     if (draggableCard.value) {
@@ -203,39 +204,32 @@ function mapClick() {
 <template>
     <main>
         <div class="map-container">
-            <l-map ref="map" v-model:zoom="zoom" v-model:center="center" @ready="setMapEvents" :min-zoom="5" @click="mapClick()"
-                :useGlobalLeaflet="false">
+            <l-map ref="map" v-model:zoom="zoom" v-model:center="center" @ready="setMapEvents" :min-zoom="5"
+                @click="mapClick()" :useGlobalLeaflet="false">
                 <l-tile-layer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"></l-tile-layer>
                 <div v-for="cleanwalk in testCleanwalkList">
-                    <l-marker @click="slideUp()" :lat-lng="L.latLng(cleanwalk.lat, cleanwalk.lng)" >
-                        <l-icon 
-                        :icon-size="[25, 41]"
-                        :icon-anchor="[12, 41]"
-                        :iconUrl="cleanwalk.isAsso ? 'https://i.ibb.co/XkyvQmm/blue-map.png' : 'https://i.ibb.co/zZjWfnp/green-map.png'"
-                        >
+                    <l-marker @click="slideUp()" :lat-lng="L.latLng(cleanwalk.lat, cleanwalk.lng)">
+                        <l-icon :icon-size="[25, 41]" :icon-anchor="[12, 41]"
+                            :iconUrl="cleanwalk.isAsso ? 'https://i.ibb.co/XkyvQmm/blue-map.png' : 'https://i.ibb.co/zZjWfnp/green-map.png'">
                         </l-icon>
                     </l-marker>
                 </div>
             </l-map>
         </div>
-        <div class="info-logo" :class="{ 'onSearch': cardListBool === true }">
-            <div class="mini-logo">
-                <img src="../assets/mini-logo.svg" alt="mini-logo" v-if="cardListBool === false">
-                <img src="../assets/logo.svg" alt="mini-logo" v-else>
-
+        <div class="top-bar">
+            <img src="../assets/logo.svg" alt="logo" v-if="!cardListBool">
+            <div class="search-bar" :class="{ 'active': cardListBool, 'base': !cardListBool }">
+                <button @click="cardListBool = false">
+                    <iconLeftArrow />
+                </button>
+                <input @click="hideCleanwalkList()" name="search" type="text" placeholder="Rechercher une cleanwalk" />
+                <label for="search" @click="cardListBool = true">
+                    <iconSearch />
+                </label>
             </div>
-            <div class="info">
-                <img src="../assets/info.svg" alt="get-infos">
-            </div>
-        </div>
-        <div class="search-bar" :class="{ 'onSearch': cardListBool === true }">
-            <button @click="cardListBool = false" :class="{ 'active': cardListBool === false }">
-                <iconLeftArrow />
+            <button class="info">
+                <iconInfo />
             </button>
-            <input @click="hideCleanwalkList()" name="search" type="text" placeholder="Rechercher une cleanwalk" />
-            <label for="search">
-                <iconSearch />
-            </label>
         </div>
         <div ref="draggableCard" class="draggable-card">
             <div class="card-handle" @click="!cleanwalkStore.cleanwalkIsSelect && (cardListBool = true);">
@@ -266,7 +260,7 @@ function mapClick() {
                 <h3>10 cleanwalks à proximité</h3>
             </div>
         </div>
-        <div class="cleanwalk-list" :class="{'active': cardListBool === false }">
+        <div class="cleanwalk-list" :class="{ 'active': cardListBool === false }">
             <div class="container" ref="cleanwalkListContainer">
                 <div v-for="cleanwalk in testCleanwalkList" :key="cleanwalk.id" class="cleanwalk">
                     <div class="title">{{ cleanwalk.title }}</div>
@@ -287,7 +281,6 @@ function mapClick() {
 </template>
 
 <style scoped lang="scss">
-
 html,
 body,
 main {
@@ -296,89 +289,109 @@ main {
     height: 100%;
     width: 100vw;
 
-    .info-logo {
+    .top-bar {
         position: fixed;
         top: 0;
         left: 0;
-        z-index: 9999;
         width: 100vw;
+        z-index: 9998;
+        background-color: var(--color-primary);
         display: flex;
-        justify-content: space-between;
+        padding: 30px 19px 20px;
+        justify-content: end;
 
-        &.onSearch {
-            background-color: var(--color-primary);
-            padding-bottom: 1px;
-        }
-        
-        div {
-            background-color: var(--color-primary);
-            display: flex;
-            align-items: center;
-            justify-content: center;
-
+        img {
+            position: absolute;
+            left: 31px;
+            width: 104px;
+            margin-top: 8px;
         }
 
         .info {
-            border-bottom-left-radius: 12px;        
-            padding: 4px 5px 4px 7px;
-        }
-        .mini-logo {
-            padding-left: 5px;
-            border-bottom-right-radius: 12px;        
-        }
-    }
+            background-color: #fff;
+            border-radius: 8px;
+            border: 1px solid #CBD5E1;
+            width: 38px;
+            height: 38px;
+            margin-left: 8px;
 
-    .search-bar {
-        position: fixed;
-        top: 60px;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        width: 90%;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        background-color: white;
-        z-index: 9998;
-        border-radius: 12px;
-        border: 1px solid #CBD5E1;
-        stroke: #CBD5E1;
+            svg {
+                stroke: #94A3B8;
+            }
 
-        &.onSearch {
-            width: 100%;
-            border-radius: 0;
         }
 
-        button {
-            padding: 10px 5px 10px 10px;
+        .search-bar {
+            &.base {
+
+                input {
+                    opacity: 0;
+                    position: absolute;
+                    width: 1px;
+                }
+                button {
+                    display: none;
+                }
+
+                background-color: #fff;
+                border-radius: 8px;
+                border: 1px solid #CBD5E1;
+                width: 38px;
+                height: 38px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+
+                svg {
+                    stroke: #94A3B8;
+                    width: 24px;
+                    height: 24px;
+                    margin: 5px 1px 0 0;
+                }
+
+            }
+
             &.active {
-                opacity: 0;
+
+                flex-grow: 1;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                background-color: white;
+                border-radius: 8px;
+                border: 1px solid #CBD5E1;
+                stroke: #CBD5E1;
+
+                button {
+                    padding: 0px 5px 0px 10px;
+                }
+
+                input {
+                    width: 100%;
+                    border: none;
+                    outline: none;
+                    font-size: 14px;
+                    font-weight: 500;
+                    color: #373646;
+                    background-color: white;
+                    padding: 10px 0;
+                }
+
+                label {
+                    padding-top: 8px;
+                    padding-right: 10px;
+                }
+
+                svg {
+                    width: 20px;
+                    height: 20px;
+                    stroke: #94A3B8;
+                }
 
             }
         }
-
-        input {
-            width: 100%;
-            padding: 10px;
-            border: none;
-            outline: none;
-            font-size: 14px;
-            font-weight: 500;
-            color: #373646;
-            background-color: white;
-            padding: 15px 0;
-        }
-        label {
-            padding-top: 8px;
-            padding-right: 10px;
-        }
-
-        svg {
-            width: 20px;
-            height: 20px;
-            stroke: #373646;
-        }
-        
     }
+
 
     .map-container {
         height: 100vh;
@@ -390,13 +403,17 @@ main {
         left: 0;
         width: 100%;
         height: 100%;
-        background-color: rgba(128, 128, 128, 0.5); /* Semi-transparent */
-        backdrop-filter: blur(5px); /* Appliquer l'effet de flou */
+        background-color: rgba(128, 128, 128, 0.5);
+        /* Semi-transparent */
+        backdrop-filter: blur(5px);
+        /* Appliquer l'effet de flou */
         z-index: 999;
         stroke: black;
+
         &.active {
             display: none;
         }
+
         .container {
             width: 100%;
             height: 100%;
@@ -414,19 +431,19 @@ main {
                 padding: 10px;
                 background-color: white;
                 width: 90%;
-    
+
                 .title {
                     font-size: 16px;
                     font-style: normal;
                     font-weight: 500;
                 }
-        
+
                 .flex {
                     display: flex;
                     align-items: center;
                     gap: 10px;
                 }
-    
+
             }
         }
 
