523afb5ad816cc773bab90ca1b6474fd87646d8e
Arthur FRIN <frin.arthur@gmail.com>
2024-04-21 18:33:52 +0200

    feat: uplad img front (draft)

---
+++ front/src/components/dragDrop.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="drop-area" @dragover.prevent="dragOver" @dragleave.prevent="dragLeave" @drop.prevent="onFileDrop"
+    @click="fileInputClick" :class="{ 'dragover': isDragOver }">
+    <input type="file" ref="fileInput" @change="onFileChange" accept="image/jpeg, image/png" style="display: none" />
+    <icon-photo class="icon-photo" v-if="!imageSrc"/>
+
+    <img v-if="imageSrc" :src="imageSrc" alt="Preview" class="preview" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, type Ref } from 'vue';
+import iconPhoto from './icons/icon-photo.vue';
+import iconCross from './icons/icon-cross.vue';
+
+const fileInput: Ref<HTMLInputElement | null> = ref(null);
+const imageSrc: Ref<string | null> = ref(null);
+const isDragOver = ref(false);
+
+const dragOver = () => {
+  isDragOver.value = true;
+};
+
+const dragLeave = () => {
+  isDragOver.value = false;
+};
+
+const onFileDrop = (event: DragEvent): void => {
+  const files = event.dataTransfer?.files;
+  if (files) {
+    processFile(files[0]);
+  }
+};
+
+const fileInputClick = () => {
+  fileInput.value?.click();
+};
+
+const onFileChange = (): void => {
+  if (fileInput.value?.files && fileInput.value.files.length > 0) {
+    const file = fileInput.value.files[0];
+    processFile(file);
+  }
+};
+
+const processFile = (file: File): void => {
+  if (file && (file.type === "image/jpeg" || file.type === "image/png")) {
+    const reader = new FileReader();
+    reader.onload = (e: ProgressEvent<FileReader>) => {
+      if (e.target?.result) {
+        imageSrc.value = e.target.result as string;
+      }
+    };
+    reader.readAsDataURL(file);
+  } else {
+    alert("Seules les images de type JPEG ou PNG sont autorisées.");
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.drop-area {
+  text-align: center;
+  margin: 20px;
+  cursor: pointer;
+  aspect-ratio: 16/9;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #E1F4F8;
+  border-radius: 8px;
+  overflow: hidden;
+}
+
+.dragover {
+  background-color: #E1F4F8;
+}
+
+.icon-photo {
+  width: 50px;
+  height: 50px;
+  stroke: #000000;
+}
+
+.preview {
+  width: 100%;
+  aspect-ratio: 16/9;
+  object-fit: cover;
+}
+</style>
