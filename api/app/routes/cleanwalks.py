4f4dd4e7eecb0cb4944907b7bb2982bae68fc5f5
Arthur FRIN <frin.arthur@gmail.com>
2024-04-29 17:01:10 +0200

    Update cleanwalks.py

--- api/app/routes/cleanwalks.py
+++ api/app/routes/cleanwalks.py
@@ -1,5 +1,7 @@
+import datetime
 from flask import Blueprint, jsonify, request
-from app.models import db, Cleanwalk
+from sqlalchemy import func, text
+from app.models import CleanwalkUser, User, db, Cleanwalk
 from app.utils import validate_api_key
 
 
@@ -19,65 +21,135 @@ def check_api_key():
 
 # Route for get Cleanwalk by id
 @cleanwalks_bp.route('/<int:cleanwalk_id>', methods=['GET'])
-def get_cleanwalk(cleanwalk_id):
-    cleanwalk = Cleanwalk.query.get(cleanwalk_id)
+def get_cleanwalk_by_id(cleanwalk_id):
+    cleanwalk = db.session.query(
+        Cleanwalk.id.label('cleanwalk_id'),
+        Cleanwalk.name.label('cleanwalk_name'),
+        Cleanwalk.pos_lat,
+        Cleanwalk.pos_long,
+        Cleanwalk.date_begin,
+        Cleanwalk.duration,
+        Cleanwalk.description,
+        Cleanwalk.address,
+        User.firstname,
+        User.lastname,
+        User.role_id,
+        User.profile_picture,
+        User.id.label('author_id')
+    ).join(
+        CleanwalkUser, Cleanwalk.id == CleanwalkUser.cleanwalk_id
+    ).join(
+        User, CleanwalkUser.user_id == User.id
+    ).filter(
+        Cleanwalk.id == cleanwalk_id,
+        CleanwalkUser.is_host == True
+    ).one_or_none()  # Retrieves one or none object, which helps in case there are no records found or just one.
 
     if cleanwalk:
         cleanwalk_data = {
-            'id': cleanwalk.id,
-            'name': cleanwalk.name,
+            'id': cleanwalk.cleanwalk_id,
+            'name': cleanwalk.cleanwalk_name,
             'pos_lat': cleanwalk.pos_lat,
             'pos_long': cleanwalk.pos_long,
-            'date_begin': cleanwalk.date_begin,
+            'date_begin': cleanwalk.date_begin.isoformat(),
             'duration': cleanwalk.duration,
             'description': cleanwalk.description,
-            'city_id': cleanwalk.city_id,
-            'address': cleanwalk.address
+            'address': cleanwalk.address,
+            'host': {
+                'author_id': cleanwalk.author_id,  # This key is missing in the original code snippet, so I added it to match the expected JSON output
+                'firstname': cleanwalk.firstname,
+                'lastname': cleanwalk.lastname,
+                'role_id': cleanwalk.role_id,
+                'profile_picture': cleanwalk.profile_picture
+            }
         }
         return jsonify(cleanwalk_data)
     else:
         return jsonify({'message': 'Cleanwalk not found'}), 404
+
     
 # Route for get all Cleanwalks
+
 @cleanwalks_bp.route('', methods=['GET'])
+
 def get_all_cleanwalks():
-    cleanwalks = Cleanwalk.query.all()
-    if cleanwalks:
-        cleanwalk_data = []
-        for cleanwalk in cleanwalks:
-            cleanwalk_data.append({
-                'id': cleanwalk.id,
-                'name': cleanwalk.name,
-                'pos_lat': cleanwalk.pos_lat,
-                'pos_long': cleanwalk.pos_long,
-                'date_begin': cleanwalk.date_begin,
-                'duration': cleanwalk.duration,
-                'description': cleanwalk.description,
-                'address': cleanwalk.address
-            })
-        return jsonify(cleanwalk_data)
-    else:
-        return jsonify({'message': 'Cleanwalks not found'}), 404
+    now = func.now()  # Use current server time; ensure that the server time zone matches your requirements
+    two_months_later = func.date_add(now, text("interval 2 month"))  # Calculate the date 2 months from now
+
+    cleanwalks = db.session.query(
+        Cleanwalk.id.label('cleanwalk_id'),
+        Cleanwalk.name.label('cleanwalk_name'),
+        Cleanwalk.pos_lat,
+        Cleanwalk.pos_long,
+        Cleanwalk.date_begin,
+        Cleanwalk.duration,
+        Cleanwalk.description,
+        Cleanwalk.address,
+        User.firstname,
+        User.lastname,
+        User.role_id,
+        User.profile_picture
+    ).join(
+        CleanwalkUser, Cleanwalk.id == CleanwalkUser.cleanwalk_id
+    ).join(
+        User, CleanwalkUser.user_id == User.id
+    ).filter(
+        CleanwalkUser.is_host == True,
+        Cleanwalk.date_begin + func.interval(Cleanwalk.duration, 'MINUTE') > now,
+        Cleanwalk.date_begin <= two_months_later
+    ).all()
+
+    cleanwalk_data = [{
+        'id': cw.cleanwalk_id,
+        'name': cw.cleanwalk_name,
+        'pos_lat': cw.pos_lat,
+        'pos_long': cw.pos_long,
+        'date_begin': cw.date_begin.isoformat(),  # Format datetime for JSON output
+        'duration': cw.duration,
+        'description': cw.description,
+        'address': cw.address,
+        'host': {
+            'firstname': cw.firstname,
+            'lastname': cw.lastname,
+            'role_id': cw.role_id,
+            'profile_picture': cw.profile_picture
+        }
+    } for cw in cleanwalks] if cleanwalks else []
+
+    return jsonify(cleanwalk_data)
 
 #------------------------------------POST------------------------------------#
 
 # Route for create a new Cleanwalk
 @cleanwalks_bp.route('', methods=['POST'])
 def create_cleanwalk():
-    new_cleanwalk_data = request.json
-    new_cleanwalk = Cleanwalk(
-        name=new_cleanwalk_data['name'],
-        pos_lat=new_cleanwalk_data['pos_lat'],
-        pos_long=new_cleanwalk_data['pos_long'],
-        date_begin=new_cleanwalk_data['date_begin'],
-        duration=new_cleanwalk_data['duration'],
-        description=new_cleanwalk_data['description'],
-        city_id=new_cleanwalk_data['city_id'],
-        address=new_cleanwalk_data['address']
-    )
-    db.session.add(new_cleanwalk)
-    db.session.commit()
-    return jsonify({'message': 'Cleanwalk created successfully'}), 201
+    try:
+        data = request.json
+        new_cleanwalk = Cleanwalk(
+            name=data['name'],
+            pos_lat=data['pos_lat'],
+            pos_long=data['pos_long'],
+            date_begin=data['date_begin'],
+            duration=data['duration'],
+            description=data['description'],
+            address=data['address']
+        )
+        db.session.add(new_cleanwalk)
+        db.session.commit()  # Commit here to get the cleanwalk_id
+        
+        new_cleanwalk_user = CleanwalkUser(
+            cleanwalk_id=new_cleanwalk.id,
+            user_id=data['user_id'],  # This needs to be provided in the request
+            is_host=True
+        )
+        db.session.add(new_cleanwalk_user)
+        db.session.commit()
+
+        return jsonify({'message': 'Cleanwalk and host association created successfully'}), 201
+
+    except Exception as e:
+        db.session.rollback()  # Roll back in case of error
+        return jsonify({'error': str(e)}), 500
 
 #------------------------------------PUT------------------------------------#
 
