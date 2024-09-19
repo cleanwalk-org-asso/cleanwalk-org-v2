93b1fe16ca32180cfb5d5c7551fc9308c5593db8
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2024-09-19 04:24:15 +0200

    style: format the text zone and add options + placeholder button
    style: positionning the elements

--- front/src/components/AddArticle.vue
+++ front/src/components/AddArticle.vue
@@ -3,23 +3,61 @@ import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
 
 let quillOptions = {
+    bounds: '#ql-editor',
+    formats: [
+        'header', 'font', 'size',
+        'bold', 'italic', 'underline', 'strike', 'blockquote',
+        'list', 'bullet', 'indent',
+        'link', 'image', 'video'
+    ],
+    modules: {
+        toolbar: [
+            [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+            [{size: []}],
+            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+            [{'list': 'ordered'}, {'list': 'bullet'}, 
+            {'indent': '-1'}, {'indent': '+1'}],
+            ['link', 'image', 'video'],
+            ['clean']
+        ]
+    },
     placeholder: 'Ecrire votre article ici...',
+    theme: 'snow',
 }
 </script>
 
 <template>
-    <div id="ql-editor">
-        <QuillEditor theme="snow" toolbar="full" :options="quillOptions"/>
+    <div id="article-creation">
+        <div id="ql-editor">
+            <QuillEditor :options="quillOptions"/>
+        </div>
+
+        <button id="pub-button">Publier</button>
     </div>
 </template>
 
 <style scoped lang="scss">
-    #ql-editor {
+    #article-creation {
+        display: grid;
         position: absolute;
-        top: 3.5rem;
-        
-        // Avoid text zone to grow when typing
-        height: calc(100vh - 3.5rem);
-        width: 100%;
+        top: 5rem;
+        left: 5%;
+        height: 90%;
+        width: 90%;
+    }
+
+    #ql-editor {
+        height: 27.5rem;
+        margin-bottom: 2rem;
+    }
+
+    #pub-button {
+        height: 3.5rem;
+        width: 50%;
+        margin: 0 auto;
+        background-color: #4CAF50;
+        color: white;
+        font-size: 1.5rem;
+        border: none;
     }
 </style>
