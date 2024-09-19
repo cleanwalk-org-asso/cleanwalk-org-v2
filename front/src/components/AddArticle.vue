dd709aec0f5e3759525cf9992c7153773038a173
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2024-09-19 03:56:50 +0200

    style: organize page as a full editor

--- front/src/components/AddArticle.vue
+++ front/src/components/AddArticle.vue
@@ -2,12 +2,24 @@
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
 
+let quillOptions = {
+    placeholder: 'Ecrire votre article ici...',
+}
 </script>
 
 <template>
-    <QuillEditor theme="snow"/>
+    <div id="ql-editor">
+        <QuillEditor theme="snow" toolbar="full" :options="quillOptions"/>
+    </div>
 </template>
 
 <style scoped lang="scss">
-
+    #ql-editor {
+        position: absolute;
+        top: 3.5rem;
+        
+        // Avoid text zone to grow when typing
+        height: calc(100vh - 3.5rem);
+        width: 100%;
+    }
 </style>
